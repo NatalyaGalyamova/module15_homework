f850acba4a5b2be89ec48d69353cdf11e152c60c
NatalyaGalyamova <peoplejoyster@gmail.com>
2024-02-14 12:46:37 -0500

    task2 done

---
+++ task2/script.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', () => {
+   alert(`Ширина экрана: ${window.screen.width}, высота экрана: ${window.screen.height}`)
+})
+
+
+
